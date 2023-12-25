11dd66ca0474ca1a48f40d8e7e369c06e5e353ca
Nadav Pinhassi <nadavpinhassi@gmail.com>
2023-12-25 12:30:23 +0200

    sdfgdf

--- Python/fileProject.py
+++ Python/fileProject.py
@@ -1,11 +1,16 @@
-sentence = input("Enter a sentence: ")
-sentenceLength=len(sentence)
+# sentence = input("Enter a sentence: ")
+# sentenceLength=len(sentence)
 
-fileName=input("Enter the file's name: ")
-fileName=f"{fileName}.txt"
+# fileName=input("Enter the file's name: ")
+# fileName=f"{fileName}.txt"
 
-with open(fileName,"w") as f:
-    f.write(sentence)
-    f.close()
+# with open(fileName,"w") as f:
+#     f.write(sentence)
+#     f.close()
 
-print(f"You've written {sentenceLength} characters to {fileName}")
+# print(f"You've written {sentenceLength} characters to {fileName}")
+
+keys=["name","age","role"]
+values=["Nadav",22,"Student"]
+person={key:value for key,value in zip(keys,values)}
+print(person)
