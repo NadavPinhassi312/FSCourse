10a5e472ac1f748b2fc9296bc9fd12b5782508e7
Nadav Pinhassi <nadavpinhassi@gmail.com>
2023-12-26 20:58:43 +0200

    sdfgd

--- Python/fileProject.py
+++ Python/fileProject.py
@@ -1,10 +1,12 @@
-class person:
-    def __init__(self, name, city):
-        self.name=name
-        self.city=city
-    
-    def getAttributes(self):
-        print(f"Hello! My name is {self.name} and I live in {self.city}.")
 
-nadav=person("Nadav","Netanya")
-nadav.getAttributes()
+
+
+
+
+
+
+
+
+
+
+
