1b4ac696e6f77b038f7b989e31b0806618d6d030
Nadav Pinhassi <nadavpinhassi@gmail.com>
2024-01-02 19:53:49 +0200

    Catching exceptions

--- Python/fileProject.py
+++ Python/fileProject.py
@@ -1,4 +1,16 @@
+person={
+    "name":"Nadav"
+}
 
+try:
+    print(person["fav_food"])
+except KeyError as e:
+    print(f'The key {e} doesnt exist.')
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+except Exception as e:
+    print(type(e))
+    print(e)
 
 
 
