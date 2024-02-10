4350aebcf0e9c9450dd540b2dd167d121bedb4cb
Nadav Pinhassi <nadavpinhassi@gmail.com>
2024-02-10 22:03:40 +0200

    data binding

--- Angular/FirstExercise/src/app/servers/servers.component.ts
+++ Angular/FirstExercise/src/app/servers/servers.component.ts
@@ -7,10 +7,22 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer=false;
-
+  serverCreationStatus="No server was created";
+  serverName="";
+  username="";
   constructor(){
     setTimeout(()=>{
       this.allowNewServer=true;
     },2000)
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus="Server was created! Name is " + this.serverName+".";
+  }
+  onUpdateServerName(event: Event){
+    this.serverName=(<HTMLInputElement>event.target).value;
+  }
+  clearName(){
+    this.username="";
   }
 }
