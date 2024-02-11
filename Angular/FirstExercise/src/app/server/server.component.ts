1d4d1c9b545755aec4a83ee9c3d87e4ebee9ca4b
Nadav Pinhassi <nadavpinhassi@gmail.com>
2024-02-11 09:51:08 +0200

    gsdfgf

--- Angular/FirstExercise/src/app/server/server.component.ts
+++ Angular/FirstExercise/src/app/server/server.component.ts
@@ -1,14 +1,22 @@
 import { Component } from "@angular/core";
+import { ServersComponent } from "../servers/servers.component";
 
 @Component({
     selector: 'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styleUrl: './server.component.css'
 })
 export class ServerComponent{
     serverID=10;
     serverStatus="offline";
 
+    constructor(){
+        this.serverStatus=Math.random() >0.5 ? 'online' : 'offline' 
+    }
     getServerStatus(){
         return this.serverStatus;
+    }
+    getColor(){
+        return this.serverStatus=='online'?"green":"red"
     }
 }
--- Angular/FirstExercise/src/app/servers/servers.component.ts
+++ Angular/FirstExercise/src/app/servers/servers.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrl: './servers.component.css'
+  styles: ['.online{background-color:white;}']
 })
 export class ServersComponent {
   allowNewServer=false;
   serverCreationStatus="No server was created";
   serverName="";
   username="";
+  serverCreated=false;
   constructor(){
     setTimeout(()=>{
       this.allowNewServer=true;
@@ -17,6 +18,7 @@ export class ServersComponent {
   }
 
   onCreateServer(){
+    this.serverCreated=true;
     this.serverCreationStatus="Server was created! Name is " + this.serverName+".";
   }
   onUpdateServerName(event: Event){
