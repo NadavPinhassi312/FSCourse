34a5886e68c9b75ab853b1b296d714be98a4c32c
Nadav Pinhassi <nadavpinhassi@gmail.com>
2024-02-28 19:21:46 +0200

    services in the project

--- Angular/CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,20 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
-  recipes:Recipe[]=[
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.cookipedia.co.uk/wiki/images/3/3d/Ready_steady_pasta_recipe_recipe.jpg'),
-    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://www.cookipedia.co.uk/wiki/images/3/3d/Ready_steady_pasta_recipe_recipe.jpg'),
-    new Recipe('A Test Recipe 3', 'This is simply a test 3', 'https://www.cookipedia.co.uk/wiki/images/3/3d/Ready_steady_pasta_recipe_recipe.jpg'),
-  ];
-  
-  @Output() recipeClicked = new EventEmitter<Recipe>();
-  
-  onRecipeClicked(currentRecipe: Recipe){
-    this.recipeClicked.emit(currentRecipe);
+export class RecipeListComponent implements OnInit{
+  recipes:Recipe[]=[]
+
+  constructor(private recipeService: RecipeService){}
+
+  ngOnInit(){
+    this.recipes = this.recipeService.getRecipes();
   }
+
+  
 }
--- Angular/CourseProject/src/app/recipes/recipe.service.ts
+++ Angular/CourseProject/src/app/recipes/recipe.service.ts
@@ -1,3 +1,16 @@
-export class RecipeService{
+import { EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
+export class RecipeService{
+    recipeSelected = new EventEmitter<Recipe>();
+    
+    private recipes:Recipe[]=[
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://www.cookipedia.co.uk/wiki/images/3/3d/Ready_steady_pasta_recipe_recipe.jpg'),
+        new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://www.cookipedia.co.uk/wiki/images/3/3d/Ready_steady_pasta_recipe_recipe.jpg'),
+        new Recipe('A Test Recipe 3', 'This is simply a test 3', 'https://www.cookipedia.co.uk/wiki/images/3/3d/Ready_steady_pasta_recipe_recipe.jpg'),
+      ];
+
+      getRecipes(){
+        return this.recipes.slice();
+      }
 }
