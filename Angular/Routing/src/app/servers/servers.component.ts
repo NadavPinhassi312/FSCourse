1dcf6afc64d7637b27b29535e27376e2a8d8cef8
Nadav Pinhassi <nadavpinhassi@gmail.com>
2024-03-05 23:12:11 +0200

    more routing!

--- Angular/Routing/src/app/servers/servers.component.ts
+++ Angular/Routing/src/app/servers/servers.component.ts
@@ -16,7 +16,7 @@ export class ServersComponent implements OnInit {
     this.servers = this.serversService.getServers();
   }
 
-  onReload(){
-    this.router.navigate(['/servers'], {relativeTo: this.route})
-  }
+  // onReload(){
+  //   this.router.navigate(['/servers'], {relativeTo: this.route})
+  // }
 }
