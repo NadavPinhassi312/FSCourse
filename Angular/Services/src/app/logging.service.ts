cf17b8365e2cc7eade2010933a8fe83fd5e80d1b
Nadav Pinhassi <nadavpinhassi@gmail.com>
2024-03-08 12:07:18 +0200

    sdfgfdg

--- Angular/Services/src/app/logging.service.ts
+++
@@ -1,8 +0,0 @@
-export class LoggingService{
-    newAccountAdded(status:string){
-        console.log("A new account was added, the status is "+status);
-    }
-    logStatusChange(status:string){
-        console.log('A server status changed, new status: '+status);
-    }
-}
