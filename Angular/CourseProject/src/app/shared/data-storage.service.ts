46684215189ae8903048be857cbe115ad3cdbe76
Nadav Pinhassi <nadavpinhassi@gmail.com>
2024-04-06 22:03:57 +0300

    sdgsdf

--- Angular/CourseProject/src/app/shared/data-storage.service.ts
+++ Angular/CourseProject/src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -17,13 +17,16 @@ export class DataStorageService {
 
     fetchRecipes() {
         return this.http.get<Recipe[]>("https://ng-course-recipe-book-6f6d5-default-rtdb.firebaseio.com/recipes.json")
-        .pipe(map(recipes => {
-            return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
-            });
-        }))
-        .subscribe(valueReturned => {
-            this.recipeService.replaceRecipeList(valueReturned)
-        });
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return {
+                        ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
+                    };
+                });
+            }),
+                tap(recipes => {
+                    this.recipeService.replaceRecipeList(recipes)
+                })
+            )
     }
 }
